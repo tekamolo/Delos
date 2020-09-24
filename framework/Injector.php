98006a4c40698b07ec575676bad08ca44f66d5e1
Alfonso Fernandez <afernandez@nord-pay.com>
2020-09-24 07:06:15 +0200

    Cleaned Injector.php

--- framework/Injector.php
+++ framework/Injector.php
@@ -198,25 +198,6 @@ class Injector
                 $this->classCollection->set($service, new $service());
             }
         }
-
-        /**
-         * I only include services having multiple parameters, perhaps models should be included too
-         */
-
-//        preg_match("/".$this->nameSpaceString."/", $service, $matches);
-//        if (!empty($matches)) {
-//            $parametersArray = array();
-//            if(!empty($reflection->getConstructor())){
-//                foreach ($reflection->getConstructor()->getParameters() as $param) {
-//                    $parametersArray[] = $this->classCollection->get(
-//                        $this->getConcretionFromInterfaceName($param,$reflection->getConstructor()->getDocComment())
-//                    );
-//                }
-//                $this->classCollection->set($service, new $service(...$parametersArray));
-//            }else{
-//                $this->classCollection->set($service, new $service());
-//            }
-//        }
     }
 
     /**
