22519d5dcdc94360ed5ca7e7e949b1bb3d0c6389
Alfonso Fernandez Ocampo <alfonso.fernandez-ocampo@mytheresa.com>
2022-02-20 22:05:05 +0100

    updated Connection.php to connect to illuminate

--- framework/Database/Connection.php
+++ framework/Database/Connection.php
@@ -13,12 +13,12 @@ class Connection
         $env = $this->getDatabaseConfiguration($environmentFile);
         $capsule = new Manager();
         $capsule->addConnection([
-            'driver' => $env['MYSQL_CONNECTION'],
-            'host' => $env['MYSQL_HOST'],
-            'port' => $env['MYSQL_PORT'],
-            'database' => $env['MYSQL_DATABASE'],
-            'username' => $env['MYSQL_USER'],
-            'password' => $env['MYSQL_PASSWORD'],
+            'driver' => $env['DATABASE'],
+            'host' => $env['DB_HOST'],
+            'port' => $env['DB_PORT'],
+            'database' => $env['DB_DATABASE'],
+            'username' => $env['DB_USERNAME'],
+            'password' => $env['DB_PASSWORD'],
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
