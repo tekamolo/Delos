59d9ee3054da537f072ebdb241dbe849c705075a
Alfonso Fernandez Ocampo <fernandez1980@gmail.com>
2022-04-07 23:03:43 +0200

    Adjusted Demo to new Directory and File ValueObjects

--- public/application.php
+++ public/application.php
@@ -1,10 +1,14 @@
 <?php
 define('DELOS_PROJECT_ROOT_PATH', dirname(dirname(__FILE__)));
 
-require_once DELOS_PROJECT_ROOT_PATH."/vendor/autoload.php";
+require_once DELOS_PROJECT_ROOT_PATH . "/vendor/autoload.php";
 
 $kernel = new \Delos\Kernel();
-$kernel->setProjectRootPath(DELOS_PROJECT_ROOT_PATH);
-$kernel->setRoutingFile('/framework/routing.xml');
-$kernel->setEnvironmentFile(DELOS_PROJECT_ROOT_PATH."/.env");
+$kernel->setProjectRootPath(
+    \Delos\Shared\Directory::createFromString(DELOS_PROJECT_ROOT_PATH)
+);
+$kernel->setRoutingFile(
+    \Delos\Shared\File::createFromString(DELOS_PROJECT_ROOT_PATH . '/framework/routing.xml')
+);
+$kernel->setEnvironmentFile(\Delos\Shared\File::createFromString(DELOS_PROJECT_ROOT_PATH . "/.env"));
 $kernel->boot();
